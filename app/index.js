8f851b217a9c27f6e8031606d4d472b55a17a18b
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2025-02-05 23:33:41 +0500

    app init

--- app/index.js
+++ app/index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+if(Text.defaultProps) {
+    Text.defaultProps.allowFontScalling = false
+} else {
+    Text.defaultProps = {}
+    Text.defaultProps.allowFontScalling = false
+}
+
+if(TextInput.defaultProps) {
+    TextInput.defaultProps.allowFontScalling = false
+} else {
+    TextInput.defaultProps = {}
+    TextInput.defaultProps.allowFontScalling = false
+}
 
 AppRegistry.registerComponent(appName, () => App);
